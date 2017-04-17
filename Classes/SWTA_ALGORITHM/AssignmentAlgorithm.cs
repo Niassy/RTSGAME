06f5232efca28907b900fbfa5f625249f7f23f63
Niassy <mamadoujbniassy@outlook.com>
2017-04-17 11:41:57 +0000

    Add files via upload

---
+++ Classes/SWTA_ALGORITHM/AssignmentAlgorithm.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AssignmentAlgorithm : MonoBehaviour {
+
+
+    public float m_TimeExecution = 5.0f;
+    protected float m_CurrentTimeExecution = 0.0f;
+
+    // only for debug
+    public float m_InitialFitness = 0.0f;
+    public float m_CurrentFitness = 0.0f;
+      
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public virtual void assignWeaponToTarget (Team other)
+    {
+
+    }
+
+    public virtual void assignWeaponToTarget(GameObject[] Weapons, GameObject[] Targets)
+    {
+
+    }
+}
